80281c540f4fd2cb551f1556f705110689bc8086
Yinan Li <liyinan926@gmail.com>
2017-09-09 16:06:05 -0700

    Added Dockerfile

--- pkg/cmd/doc.go
+++
@@ -1,5 +0,0 @@
-package cmd
-
-// Package cmd implements the command-line utility for users who have local files to
-// stage as part of the submission process and cannot have the custom controller submit
-// applications on their behalf.
