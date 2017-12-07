f348bf569d9e4326e8d73dd16c74926872b838a2
Yinan Li <liyinan926@gmail.com>
2017-12-07 15:54:02 -0800

    Added a check for the kube-dns add-on

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
@@ -14,29 +15,41 @@ import (
 
 	apiextensionsclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	clientset "k8s.io/client-go/kubernetes"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
-	kubeconfig := flag.String("kubeconfig", "", "Path to a kube config. Only required if out-of-cluster.")
+	kubeConfig := flag.String("kubeConfig", "", "Path to a kube config. Only required if out-of-cluster.")
 	initializerThreads := flag.Int("initializer-threads", 10, "Number of worker threads used by the initializer controller.")
 	submissionRunnerThreads := flag.Int("submission-threads", 3, "Number of worker threads used by the submission runner.")
 	flag.Parse()
 
-	glog.Info("Starting the Spark operator")
-
-	// Create the client config. Use kubeconfig if given, otherwise assume in-cluster.
-	config, err := buildConfig(*kubeconfig)
+	// Create the client config. Use kubeConfig if given, otherwise assume in-cluster.
+	config, err := buildConfig(*kubeConfig)
 	if err != nil {
 		panic(err)
 	}
-
 	kubeClient, err := clientset.NewForConfig(config)
 	if err != nil {
 		panic(err)
 	}
+
+	glog.Info("Checking the kube-dns add-on")
+	if err = checkKubeDNS(kubeClient); err != nil {
+		return
+	}
+
+	glog.Info("Starting the Spark operator")
+
+	initializerController := initializer.New(kubeClient)
+	stopCh := make(chan struct{})
+	errCh := make(chan error)
+	go initializerController.Run(*initializerThreads, stopCh, errCh)
+
 	crdClient, err := crd.NewClient(config)
 	if err != nil {
 		panic(err)
@@ -45,12 +58,6 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
-	stopCh := make(chan struct{})
-	errCh := make(chan error)
-
-	initializerController := initializer.New(kubeClient)
-	go initializerController.Run(*initializerThreads, stopCh, errCh)
 
 	sparkApplicationController := controller.New(crdClient, kubeClient, apiExtensionsClient, *submissionRunnerThreads)
 	go sparkApplicationController.Run(stopCh, errCh)
@@ -69,9 +76,29 @@ func main() {
 	}
 }
 
-func buildConfig(kubeconfig string) (*rest.Config, error) {
-	if kubeconfig != "" {
-		return clientcmd.BuildConfigFromFlags("", kubeconfig)
+func buildConfig(kubeConfig string) (*rest.Config, error) {
+	if kubeConfig != "" {
+		return clientcmd.BuildConfigFromFlags("", kubeConfig)
 	}
 	return rest.InClusterConfig()
+}
+
+func checkKubeDNS(kubeClient clientset.Interface) error {
+	endpoints, err := kubeClient.CoreV1().Endpoints("kube-system").Get("kube-dns", metav1.GetOptions{})
+	if err != nil {
+		if errors.IsNotFound(err) {
+			glog.Error("no endpoints for kube-dns found in namespace kube-system")
+		} else {
+			glog.Errorf("failed to get endpoints for kube-dns in namespace kube-system: %v", err)
+		}
+		glog.Error("cluster add-on kube-dns is required to run Spark applications")
+		return err
+	}
+
+	if len(endpoints.Subsets) == 0 {
+		glog.Error("cluster add-on kube-dns is required to run Spark applications")
+		return fmt.Errorf("no endpoints for kube-dns available in namespace kube-system")
+	}
+
+	return nil
 }
