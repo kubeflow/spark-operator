fb7d148c41c063292aeb181604c4f2462f5905c6
Anirudh <ramanathana@google.com>
2018-03-26 20:57:57 -0700

    test framework

---
+++ test/e2e/framework/framework.go
@@ -0,0 +1,143 @@
+// Copyright 2016 The prometheus-operator & Kubernetes Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package framework
+
+import (
+	"fmt"
+	"time"
+
+	"k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/client-go/kubernetes"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/spark-on-k8s-operator/test/e2e/testutil"
+
+	"github.com/pkg/errors"
+)
+
+// Framework contains all components required to run the test framework.
+type Framework struct {
+	KubeClient     kubernetes.Interface
+	MasterHost     string
+	Namespace      *v1.Namespace
+	OperatorPod    *v1.Pod
+	DefaultTimeout time.Duration
+}
+
+// Setup setups a test framework and returns it.
+func New(ns, kubeconfig, opImage string) (*Framework, error) {
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		fmt.Println(err, "build config from flags failed")
+	}
+
+	cli, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		fmt.Println(err, "creating new kube-client failed")
+	}
+
+	namespace, err := testutil.CreateNamespace(cli, ns)
+	if err != nil {
+		fmt.Println(nil, err, namespace)
+	}
+
+	f := &Framework{
+		MasterHost:     config.Host,
+		KubeClient:     cli,
+		Namespace:      namespace,
+		DefaultTimeout: time.Minute,
+	}
+
+	err = f.Setup(opImage)
+	if err != nil {
+		return nil, errors.Wrap(err, "setup test environment failed")
+	}
+
+	return f, nil
+}
+
+func (f *Framework) Setup(opImage string) error {
+	if err := f.setupOperator(opImage); err != nil {
+		return errors.Wrap(err, "setup operator failed")
+	}
+
+	return nil
+}
+
+func (f *Framework) setupOperator(opImage string) error {
+	if _, err := testutil.CreateServiceAccount(f.KubeClient, f.Namespace.Name, "../../manifest/spark-operator-rbac.yaml"); err != nil && !apierrors.IsAlreadyExists(err) {
+		return errors.Wrap(err, "failed to create operator service account")
+	}
+
+	if err := testutil.CreateClusterRole(f.KubeClient, "../../manifest/spark-operator-rbac.yaml"); err != nil && !apierrors.IsAlreadyExists(err) {
+		return errors.Wrap(err, "failed to create cluster role")
+	}
+
+	if _, err := testutil.CreateClusterRoleBinding(f.KubeClient, f.Namespace.Name, "../../manifest/spark-operator-rbac.yaml"); err != nil && !apierrors.IsAlreadyExists(err) {
+		return errors.Wrap(err, "failed to create cluster role binding")
+	}
+
+	deploy, err := testutil.MakeDeployment("../../manifest/spark-operator.yaml")
+	if err != nil {
+		return err
+	}
+
+	if opImage != "" {
+		// Override operator image used, if specified when running tests.
+		deploy.Spec.Template.Spec.Containers[0].Image = opImage
+	}
+
+	err = testutil.CreateDeployment(f.KubeClient, f.Namespace.Name, deploy)
+	if err != nil {
+		return err
+	}
+
+	opts := metav1.ListOptions{LabelSelector: fields.SelectorFromSet(fields.Set(deploy.Spec.Template.ObjectMeta.Labels)).String()}
+	err = testutil.WaitForPodsReady(f.KubeClient, f.Namespace.Name, f.DefaultTimeout, 1, opts)
+	if err != nil {
+		return errors.Wrap(err, "failed to wait for operator to become ready")
+	}
+
+	pl, err := f.KubeClient.Core().Pods(f.Namespace.Name).List(opts)
+	if err != nil {
+		return err
+	}
+	f.OperatorPod = &pl.Items[0]
+	return nil
+}
+
+// Teardown tears down a previously initialized test environment.
+func (f *Framework) Teardown() error {
+	if err := testutil.DeleteClusterRole(f.KubeClient, "../../manifest/spark-operator-rbac.yaml"); err != nil && !apierrors.IsAlreadyExists(err) {
+		return errors.Wrap(err, "failed to delete operator cluster role")
+	}
+
+	if err := testutil.DeleteClusterRoleBinding(f.KubeClient, "../../manifest/spark-operator-rbac.yaml"); err != nil && !apierrors.IsAlreadyExists(err) {
+		return errors.Wrap(err, "failed to delete operator cluster role binding")
+	}
+
+	if err := f.KubeClient.Extensions().Deployments(f.Namespace.Name).Delete("sparkoperator", nil); err != nil {
+		return err
+	}
+
+	if err := testutil.DeleteNamespace(f.KubeClient, f.Namespace.Name); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ test/e2e/testutil/operator.go
@@ -0,0 +1,188 @@
+// Copyright 2016 The prometheus-operator Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package testutil
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"time"
+
+	// crdutils "github.com/ant31/crd-validation/pkg"
+	version "github.com/hashicorp/go-version"
+	"github.com/pkg/errors"
+	"k8s.io/api/core/v1"
+	// extensionsobj "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/discovery"
+	clientv1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	"k8s.io/client-go/rest"
+)
+
+// CustomResourceDefinitionTypeMeta set the default kind/apiversion of CRD
+var CustomResourceDefinitionTypeMeta metav1.TypeMeta = metav1.TypeMeta{
+	Kind:       "CustomResourceDefinition",
+	APIVersion: "apiextensions.k8s.io/v1beta1",
+}
+
+// WaitForCRDReady waits for a third party resource to be available for use.
+func WaitForCRDReady(listFunc func(opts metav1.ListOptions) (runtime.Object, error)) error {
+	err := wait.Poll(3*time.Second, 10*time.Minute, func() (bool, error) {
+		_, err := listFunc(metav1.ListOptions{})
+		if err != nil {
+			if se, ok := err.(*apierrors.StatusError); ok {
+				if se.Status().Code == http.StatusNotFound {
+					return false, nil
+				}
+			}
+			return false, err
+		}
+		return true, nil
+	})
+
+	return errors.Wrap(err, fmt.Sprintf("timed out waiting for Custom Resource"))
+}
+
+// PodRunningAndReady returns whether a pod is running and each container has
+// passed it's ready state.
+func PodRunningAndReady(pod v1.Pod) (bool, error) {
+	switch pod.Status.Phase {
+	case v1.PodFailed, v1.PodSucceeded:
+		return false, fmt.Errorf("pod completed")
+	case v1.PodRunning:
+		for _, cond := range pod.Status.Conditions {
+			if cond.Type != v1.PodReady {
+				continue
+			}
+			return cond.Status == v1.ConditionTrue, nil
+		}
+		return false, fmt.Errorf("pod ready condition not found")
+	}
+	return false, nil
+}
+
+func NewClusterConfig(host string, tlsInsecure bool, tlsConfig *rest.TLSClientConfig) (*rest.Config, error) {
+	var cfg *rest.Config
+	var err error
+
+	if len(host) == 0 {
+		if cfg, err = rest.InClusterConfig(); err != nil {
+			return nil, err
+		}
+	} else {
+		cfg = &rest.Config{
+			Host: host,
+		}
+		hostURL, err := url.Parse(host)
+		if err != nil {
+			return nil, fmt.Errorf("error parsing host url %s : %v", host, err)
+		}
+		if hostURL.Scheme == "https" {
+			cfg.TLSClientConfig = *tlsConfig
+			cfg.Insecure = tlsInsecure
+		}
+	}
+	cfg.QPS = 100
+	cfg.Burst = 100
+
+	return cfg, nil
+}
+
+func IsResourceNotFoundError(err error) bool {
+	se, ok := err.(*apierrors.StatusError)
+	if !ok {
+		return false
+	}
+	if se.Status().Code == http.StatusNotFound && se.Status().Reason == metav1.StatusReasonNotFound {
+		return true
+	}
+	return false
+}
+
+func CreateOrUpdateService(sclient clientv1.ServiceInterface, svc *v1.Service) error {
+	service, err := sclient.Get(svc.Name, metav1.GetOptions{})
+	if err != nil && !apierrors.IsNotFound(err) {
+		return errors.Wrap(err, "retrieving service object failed")
+	}
+
+	if apierrors.IsNotFound(err) {
+		_, err = sclient.Create(svc)
+		if err != nil {
+			return errors.Wrap(err, "creating service object failed")
+		}
+	} else {
+		svc.ResourceVersion = service.ResourceVersion
+		_, err := sclient.Update(svc)
+		if err != nil && !apierrors.IsNotFound(err) {
+			return errors.Wrap(err, "updating service object failed")
+		}
+	}
+
+	return nil
+}
+
+func CreateOrUpdateEndpoints(eclient clientv1.EndpointsInterface, eps *v1.Endpoints) error {
+	endpoints, err := eclient.Get(eps.Name, metav1.GetOptions{})
+	if err != nil && !apierrors.IsNotFound(err) {
+		return errors.Wrap(err, "retrieving existing kubelet endpoints object failed")
+	}
+
+	if apierrors.IsNotFound(err) {
+		_, err = eclient.Create(eps)
+		if err != nil {
+			return errors.Wrap(err, "creating kubelet endpoints object failed")
+		}
+	} else {
+		eps.ResourceVersion = endpoints.ResourceVersion
+		_, err = eclient.Update(eps)
+		if err != nil {
+			return errors.Wrap(err, "updating kubelet endpoints object failed")
+		}
+	}
+
+	return nil
+}
+
+// GetMinorVersion returns the minor version as an integer
+func GetMinorVersion(dclient discovery.DiscoveryInterface) (int, error) {
+	v, err := dclient.ServerVersion()
+	if err != nil {
+		return 0, err
+	}
+
+	ver, err := version.NewVersion(v.String())
+	if err != nil {
+		return 0, err
+	}
+
+	return ver.Segments()[1], nil
+}
+
+// func NewCustomResourceDefinition(crdKind monitoringv1.CrdKind, group string, labels map[string]string, validation bool) *extensionsobj.CustomResourceDefinition {
+// 	return crdutils.NewCustomResourceDefinition(crdutils.Config{
+// 		SpecDefinitionName:    crdKind.SpecName,
+// 		EnableValidation:      validation,
+// 		Labels:                crdutils.Labels{LabelsMap: labels},
+// 		ResourceScope:         string(extensionsobj.NamespaceScoped),
+// 		Group:                 group,
+// 		Kind:                  crdKind.Kind,
+// 		Version:               monitoringv1.Version,
+// 		Plural:                crdKind.Plural,
+// 		GetOpenAPIDefinitions: monitoringv1.GetOpenAPIDefinitions,
+// 	})
+// }
