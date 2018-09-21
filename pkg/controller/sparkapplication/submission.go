dedbae2ca41ee609b27fcac8b1ac8449899ff1c1
Anmol Khurana <akhurana@lyft.com>
2018-09-21 00:42:55 -0700

    Clean-up

--- pkg/controller/sparkapplication/submission.go
+++ pkg/controller/sparkapplication/submission.go
@@ -143,14 +143,10 @@ func buildSubmissionCommandArgs(app *v1beta1.SparkApplication, submissionID stri
 			*app.Spec.HadoopConfigMap))
 	}
 
-	if app.Spec.SparkConf != nil {
-		// Add Spark configuration properties.
-		for key, value := range app.Spec.SparkConf {
-			args = append(args, "--conf", fmt.Sprintf("%s=%s", key, value))
-		}
+	// Add Spark configuration properties.
+	for key, value := range app.Spec.SparkConf {
+		args = append(args, "--conf", fmt.Sprintf("%s=%s", key, value))
 	}
-	// Operator triggered spark-submit should never wait for App completion
-	args = append(args, "--conf", fmt.Sprintf("%s=%s", config.SparkWaitAppCompletion, "false"))
 
 	// Add Hadoop configuration properties.
 	for key, value := range app.Spec.HadoopConf {
