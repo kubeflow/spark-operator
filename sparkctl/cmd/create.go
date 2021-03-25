dce3d62b5c5209fe273d8f71d046fb58b68ca157
Faisal Chaudhry <chaudhryfaisal@users.noreply.github.com>
2021-03-24 22:25:28 -0500

    added s3-force-path-style to choose whether to force path style URLs for S3 objects (#1206)

--- sparkctl/cmd/create.go
+++ sparkctl/cmd/create.go
@@ -46,6 +46,7 @@ var UploadToPath string
 var UploadToEndpoint string
 var UploadToRegion string
 var Public bool
+var S3ForcePathStyle bool
 var Override bool
 var From string
 
@@ -99,6 +100,8 @@ func init() {
 		"https://storage.googleapis.com", "the GCS or S3 storage api endpoint url")
 	createCmd.Flags().BoolVarP(&Public, "public", "c", false,
 		"whether to make uploaded files publicly available")
+	createCmd.Flags().BoolVarP(&S3ForcePathStyle, "s3-force-path-style", "p", false,
+		"whether to force path style URLs for S3 objects")
 	createCmd.Flags().BoolVarP(&Override, "override", "o", false,
 		"whether to override remote files with the same names")
 	createCmd.Flags().StringVarP(&From, "from", "f", "",
@@ -381,7 +384,7 @@ func uploadLocalDependencies(app *v1beta2.SparkApplication, files []string) ([]s
 	case "gs":
 		uh, err = newGCSBlob(ctx, uploadBucket, UploadToEndpoint, UploadToRegion)
 	case "s3":
-		uh, err = newS3Blob(ctx, uploadBucket, UploadToEndpoint, UploadToRegion)
+		uh, err = newS3Blob(ctx, uploadBucket, UploadToEndpoint, UploadToRegion, S3ForcePathStyle)
 	default:
 		return nil, fmt.Errorf("unsupported upload location URL scheme: %s", uploadLocationUrl.Scheme)
 	}
