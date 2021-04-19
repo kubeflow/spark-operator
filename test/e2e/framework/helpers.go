426a68bb1162efa09c17c85d5c626a93f8169c06
Josh Soref <jsoref@users.noreply.github.com>
2021-04-19 14:40:27 -0400

    spelling: relative
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- test/e2e/framework/helpers.go
+++ test/e2e/framework/helpers.go
@@ -34,10 +34,10 @@ import (
 )
 
 // PathToOSFile gets the absolute path from relative path.
-func PathToOSFile(relativPath string) (*os.File, error) {
-	path, err := filepath.Abs(relativPath)
+func PathToOSFile(relativePath string) (*os.File, error) {
+	path, err := filepath.Abs(relativePath)
 	if err != nil {
-		return nil, errors.Wrap(err, fmt.Sprintf("failed generate absolute file path of %s", relativPath))
+		return nil, errors.Wrap(err, fmt.Sprintf("failed generate absolute file path of %s", relativePath))
 	}
 
 	manifest, err := os.Open(path)
