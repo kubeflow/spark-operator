91ab94a6142b2801beb97227a9724a46da0fced4
Jacob Salway <jacob.salway@rokt.com>
2024-06-10 11:00:48 +1000

    WIP

---
+++ pkg/batchscheduler/yunikorn/utils_test.go
@@ -0,0 +1,44 @@
+package yunikorn
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestMergeMaps(t *testing.T) {
+	testCases := []struct {
+		m1       map[string]string
+		m2       map[string]string
+		expected map[string]string
+	}{
+		{
+			m1:       map[string]string{},
+			m2:       map[string]string{},
+			expected: map[string]string{},
+		},
+		{
+			m1:       map[string]string{"key1": "value1"},
+			m2:       map[string]string{},
+			expected: map[string]string{"key1": "value1"},
+		},
+		{
+			m1:       map[string]string{},
+			m2:       map[string]string{"key1": "value1"},
+			expected: map[string]string{"key1": "value1"},
+		},
+		{
+			m1:       map[string]string{"key1": "value1"},
+			m2:       map[string]string{"key2": "value2"},
+			expected: map[string]string{"key1": "value1", "key2": "value2"},
+		},
+		{
+			m1:       map[string]string{"key1": "value1"},
+			m2:       map[string]string{"key1": "value2", "key2": "value2"},
+			expected: map[string]string{"key1": "value2", "key2": "value2"},
+		},
+	}
+
+	for _, tc := range testCases {
+		assert.Equal(t, tc.expected, mergeMaps(tc.m1, tc.m2))
+	}
+}
--- pkg/batchscheduler/yunikorn/yunikorn_scheduler.go
+++ pkg/batchscheduler/yunikorn/yunikorn_scheduler.go
@@ -1,6 +1,8 @@
 package yunikorn
 
 import (
+	"encoding/json"
+	"fmt"
 	"k8s.io/client-go/rest"
 
 	"github.com/apache/yunikorn-k8shim/pkg/cache"
@@ -12,6 +14,11 @@ import (
 const (
 	DriverTaskGroupName   = "spark-driver"
 	ExecutorTaskGroupName = "spark-executor"
+
+	TaskGroupNameAnnotation = "yunikorn.apache.org/task-group-name"
+	TaskGroupsAnnotation    = "yunikorn.apache.org/task-groups"
+
+	QueueLabel = "queue"
 )
 
 type YunikornBatchScheduler struct{}
@@ -33,31 +40,41 @@ func (y *YunikornBatchScheduler) DoBatchSchedulingOnSubmission(app *v1beta2.Spar
 		{
 			Name:         DriverTaskGroupName,
 			MinMember:    1,
-			MinResource:  nil,
-			NodeSelector: app.Spec.Driver.NodeSelector,
+			MinResource:  nil, // TODO
+			NodeSelector: mergeMaps(app.Spec.NodeSelector, app.Spec.Driver.NodeSelector),
 			Tolerations:  app.Spec.Driver.Tolerations,
 			Affinity:     app.Spec.Driver.Affinity,
 			Labels:       app.Spec.Driver.Labels,
 		},
 	}
 
-	if initialExecutors := getInitialExecutors(app); initialExecutors > 0 {
+	// A minMember of zero is not a valid config for a Yunikorn task group,
+	// so we should leave out the executor task group completely
+	// if the initial number of executors is zero
+	initialExecutors := getInitialExecutors(app)
+	if initialExecutors > 0 {
 		taskGroups = append(taskGroups, cache.TaskGroup{
 			Name:         ExecutorTaskGroupName,
 			MinMember:    initialExecutors,
-			MinResource:  nil,
-			NodeSelector: app.Spec.Executor.NodeSelector,
+			MinResource:  nil, // TODO
+			NodeSelector: mergeMaps(app.Spec.NodeSelector, app.Spec.Executor.NodeSelector),
 			Tolerations:  app.Spec.Executor.Tolerations,
 			Affinity:     app.Spec.Executor.Affinity,
 			Labels:       app.Spec.Executor.Labels,
 		})
 	}
 
+	if err := addTaskGroupAnnotations(app, taskGroups); err != nil {
+		return fmt.Errorf("failed to add task group annotations: %v", err)
+	}
+
+	addQueueLabels(app)
+
 	return nil
 }
 
 func getInitialExecutors(app *v1beta2.SparkApplication) int32 {
-	// https://github.com/apache/spark/blob/bc187013da821eba0ffff2408991e8ec6d2749fe/core/src/main/scala/org/apache/spark/util/Utils.scala#L2539-L2542
+	// Logic copied from https://github.com/apache/spark/blob/bc187013da821eba0ffff2408991e8ec6d2749fe/core/src/main/scala/org/apache/spark/util/Utils.scala#L2539-L2542
 	initialExecutors := int32(0)
 
 	if app.Spec.Executor.Instances != nil {
@@ -74,4 +91,41 @@ func getInitialExecutors(app *v1beta2.SparkApplication) int32 {
 	}
 
 	return initialExecutors
+}
+
+func addTaskGroupAnnotations(app *v1beta2.SparkApplication, taskGroups []cache.TaskGroup) error {
+	marshalledTaskGroups, err := json.Marshal(taskGroups)
+	if err != nil {
+		return fmt.Errorf("failed to marshal taskGroups: %v", err)
+	}
+
+	if app.Spec.Driver.Annotations == nil {
+		app.Spec.Driver.Annotations = make(map[string]string)
+	}
+
+	if app.Spec.Executor.Annotations == nil {
+		app.Spec.Executor.Annotations = make(map[string]string)
+	}
+
+	app.Spec.Driver.Annotations[TaskGroupNameAnnotation] = DriverTaskGroupName
+	app.Spec.Executor.Annotations[TaskGroupNameAnnotation] = ExecutorTaskGroupName
+
+	app.Spec.Driver.Annotations[TaskGroupsAnnotation] = string(marshalledTaskGroups)
+
+	return nil
+}
+
+func addQueueLabels(app *v1beta2.SparkApplication) {
+	if app.Spec.BatchSchedulerOptions != nil && app.Spec.BatchSchedulerOptions.Queue != nil {
+		if app.Spec.Driver.Labels == nil {
+			app.Spec.Driver.Labels = make(map[string]string)
+		}
+
+		if app.Spec.Executor.Labels == nil {
+			app.Spec.Executor.Labels = make(map[string]string)
+		}
+
+		app.Spec.Driver.Labels[QueueLabel] = *app.Spec.BatchSchedulerOptions.Queue
+		app.Spec.Executor.Labels[QueueLabel] = *app.Spec.BatchSchedulerOptions.Queue
+	}
 }
