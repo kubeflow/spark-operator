25fb23f3834dce4023050981e3de7c35c2c43854
Kevin.Wu <kevin.wu@momenta.ai>
2024-08-29 23:44:02 +0800

    feat: add adjust pointer if is nil
    Signed-off-by: Kevin.Wu <kevin.wu@momenta.ai>

--- internal/webhook/sparkpod_defaulter.go
+++ internal/webhook/sparkpod_defaulter.go
@@ -478,9 +478,9 @@ func addSchedulerName(pod *corev1.Pod, app *v1beta2.SparkApplication) error {
 func addPriorityClassName(pod *corev1.Pod, app *v1beta2.SparkApplication) error {
 	var priorityClassName *string
 
-	if util.IsDriverPod(pod) && *app.Spec.Driver.PriorityClassName != "" {
+	if util.IsDriverPod(pod) && app.Spec.Driver.PriorityClassName != nil && *app.Spec.Driver.PriorityClassName != "" {
 		priorityClassName = app.Spec.Driver.PriorityClassName
-	} else if util.IsExecutorPod(pod) && *app.Spec.Executor.PriorityClassName != "" {
+	} else if util.IsExecutorPod(pod) && app.Spec.Executor.PriorityClassName != nil && *app.Spec.Executor.PriorityClassName != "" {
 		priorityClassName = app.Spec.Executor.PriorityClassName
 	} else if app.Spec.BatchSchedulerOptions != nil {
 		priorityClassName = app.Spec.BatchSchedulerOptions.PriorityClassName
