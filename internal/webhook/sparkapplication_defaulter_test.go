77325a7b69955bf8a850a5a48cb2ff4ab9287cf7
Thomas Newton <thomas.w.newton@gmail.com>
2024-10-14 00:56:10 +0100

    Still working on tests
    Signed-off-by: Thomas Newton <thomas.w.newton@gmail.com>

---
+++ internal/webhook/sparkapplication_defaulter_test.go
@@ -0,0 +1,160 @@
+/*
+Copyright 2018 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package webhook
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"testing"
+
+	// "github.com/evanphx/json-patch"
+	// "github.com/stretchr/testify/assert"
+	// corev1 "k8s.io/api/core/v1"
+	// "k8s.io/apimachinery/pkg/api/resource"
+	// metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+
+	// "gomodules.xyz/jsonpatch/v2"
+
+	"github.com/kubeflow/spark-operator/api/v1beta2"
+	"gopkg.in/yaml.v2"
+
+	// "github.com/kubeflow/spark-operator/pkg/common"
+	jsonpatch "github.com/evanphx/json-patch"
+	"github.com/getkin/kin-openapi/openapi3"
+	"k8s.io/kube-openapi/pkg/validation/validate"
+)
+
+func readYamlAndConvertToJSON(path string) ([]byte, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open file: %w", err)
+	}
+
+	var unmarshalledData map[string]interface{}
+	fileData, err := io.ReadAll(file)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file: %w", err)
+	}
+	err = yaml.Unmarshal(fileData, &unmarshalledData)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal YAML: %w", err)
+	}
+
+	jsonBytes, err := json.Marshal(unmarshalledData)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal JSON: %w", err)
+	}
+
+	return jsonBytes, nil
+}
+
+func TestDefaultSparkApplicationSparkUIOptions(t *testing.T) {
+	rawJSON := []byte(`{
+		"apiVersion": "sparkoperator.k8s.io/v1beta2",
+		"kind": "SparkApplication",
+		"metadata": {
+		  "name": "test"
+		},
+		"spec": {
+		  "image": "dummy",
+		  "mainApplicationFile": "dummy",
+		  "mode": "cluster",
+		  "type": "Python",
+		  "sparkUIOptions": {
+			"ingressTLS": [
+			  {
+				"hosts": [
+				  "*.dummy"
+				],
+				"secretName": "spark-ui-tls-secret"
+			  }
+			]
+		  },
+		  "sparkVersion": "3.5.0"
+		}
+	  }`)
+
+	ctx := context.TODO()
+	request := new(admission.Request)
+	request.Object = runtime.RawExtension{Raw: rawJSON}
+	webhook := admission.WithCustomDefaulter(runtime.NewScheme(), &v1beta2.SparkApplication{}, NewSparkApplicationDefaulter())
+	response := webhook.Handle(ctx, *request)
+
+	for _, patch := range response.Patches {
+		jsonpatch.MergePatch(rawJSON, []byte(patch.Json()))
+	}
+
+	// data, err := os.ReadFile("/home/tomnewton/spark_operator_private/charts/spark-operator-chart/crds/sparkoperator.k8s.io_sparkapplications.yaml")
+	loader := openapi3.NewLoader()
+	schema, _ := loader.LoadFromFile("/home/tomnewton/spark_operator_private/charts/spark-operator-chart/crds/sparkoperator.k8s.io_sparkapplications.yaml")
+
+	schema.NewSchema()
+	// example := openapi3.NewExample(rawJSON)
+	// err = example.Validate(ctx, openapi3.ValidationOption(schema))
+	specValidator := validate.NewSchemaValidator(&schema, nil, "", nil)
+	_ = specValidator.Validate(rawJSON)
+
+	
+}
+
+// func TestPatchSparkPod_OwnerReference(t *testing.T) {
+// 	app := &v1beta2.SparkApplication{
+// 		ObjectMeta: metav1.ObjectMeta{
+// 			Name: "spark-test",
+// 			UID:  "spark-test-1",
+// 		},
+// 	}
+
+// 	pod := &corev1.Pod{
+// 		ObjectMeta: metav1.ObjectMeta{
+// 			Name: "spark-driver",
+// 			Labels: map[string]string{
+// 				common.LabelSparkRole:               common.SparkRoleDriver,
+// 				common.LabelLaunchedBySparkOperator: "true",
+// 			},
+// 		},
+// 		Spec: corev1.PodSpec{
+// 			Containers: []corev1.Container{
+// 				{
+// 					Name:  common.SparkDriverContainerName,
+// 					Image: "spark-driver:latest",
+// 				},
+// 			},
+// 		},
+// 	}
+
+// 	// Test patching a pod without existing OwnerReference and Volume.
+// 	modifiedPod, err := getModifiedPod(pod, app)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	assert.Len(t, modifiedPod.OwnerReferences, 1)
+
+// 	// Test patching a pod with existing OwnerReference and Volume.
+// 	pod.OwnerReferences = append(pod.OwnerReferences, metav1.OwnerReference{Name: "owner-reference1"})
+
+// 	modifiedPod, err = getModifiedPod(pod, app)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	assert.Len(t, modifiedPod.OwnerReferences, 2)
+// }
