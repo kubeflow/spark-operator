d10b8f5f3a5d9f7bfb0d1e2f8d78a9bd7228fcf4
jbhalodia-slack <jbhalodia@salesforce.com>
2025-02-19 23:03:42 -0500

    Make image optional (#2439)
    * Make app.Spec.Driver.Image and app.Spec.Image optional
    Signed-off-by: jbhalodia-slack <jbhalodia@salesforce.com>
    
    * Make app.Spec.Executor.Image optional
    Signed-off-by: jbhalodia-slack <jbhalodia@salesforce.com>

--- internal/controller/sparkapplication/submission.go
+++ internal/controller/sparkapplication/submission.go
@@ -320,8 +320,6 @@ func driverConfOption(app *v1beta2.SparkApplication) ([]string, error) {
 	} else if app.Spec.Image != nil && *app.Spec.Image != "" {
 		args = append(args, "--conf",
 			fmt.Sprintf("%s=%s", common.SparkKubernetesDriverContainerImage, *app.Spec.Image))
-	} else {
-		return nil, fmt.Errorf("driver container image is not specified")
 	}
 
 	if app.Spec.Driver.Cores != nil {
@@ -676,8 +674,6 @@ func executorConfOption(app *v1beta2.SparkApplication) ([]string, error) {
 	} else if app.Spec.Image != nil && *app.Spec.Image != "" {
 		args = append(args, "--conf",
 			fmt.Sprintf("%s=%s", common.SparkKubernetesExecutorContainerImage, *app.Spec.Image))
-	} else {
-		return nil, fmt.Errorf("executor container image is not specified")
 	}
 
 	if app.Spec.Executor.Cores != nil {
