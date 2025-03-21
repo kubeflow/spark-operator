a69ca4aeaf367b7ab8d6431168de928efde253f4
TJ Miller <millert@us.ibm.com>
2025-03-21 10:51:49 -0700

    fix: add webhook cert validity checking
    Signed-off-by: TJ Miller <millert@us.ibm.com>

--- pkg/certificate/certificate_test.go
+++ pkg/certificate/certificate_test.go
@@ -18,6 +18,10 @@ package certificate_test
 
 import (
 	"context"
+	"crypto/rand"
+	"crypto/x509"
+	"encoding/pem"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -124,14 +128,16 @@ var _ = Describe("Certificate Provider", func() {
 			Name:      secretName,
 			Namespace: secretNamespace,
 		}
-		secret := &corev1.Secret{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      secretName,
-				Namespace: secretNamespace,
-			},
-		}
+		var secret *corev1.Secret
 
 		BeforeEach(func() {
+			secret = &corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      secretName,
+					Namespace: secretNamespace,
+				},
+			}
+
 			By("Creating a new webhook secret with data populated")
 			Expect(k8sClient.Create(ctx, secret)).To(Succeed())
 
@@ -170,6 +176,36 @@ var _ = Describe("Certificate Provider", func() {
 			serverCert, err := cp.ServerCert()
 			Expect(err).To(BeNil())
 			Expect(serverCert).To(Equal(secret.Data[common.ServerCertPem]))
+		})
+
+		It("Should generate a new certificate when validation fails", func() {
+			By("Creating a certificate with an expired NotAfter field")
+			Expect(k8sClient.Get(ctx, key, secret)).To(Succeed())
+			caKeyPem, _ := pem.Decode(secret.Data[common.CAKeyPem])
+			caCertPem, _ := pem.Decode(secret.Data[common.CACertPem])
+			serverKeyPem, _ := pem.Decode(secret.Data[common.ServerKeyPem])
+			serverCertPem, _ := pem.Decode(secret.Data[common.ServerCertPem])
+			caKey, _ := x509.ParsePKCS1PrivateKey(caKeyPem.Bytes)
+			caCert, _ := x509.ParseCertificate(caCertPem.Bytes)
+			serverKey, _ := x509.ParsePKCS1PrivateKey(serverKeyPem.Bytes)
+			serverCert, _ := x509.ParseCertificate(serverCertPem.Bytes)
+
+			serverCert.NotAfter = time.Now().AddDate(0, 0, -1)
+			certBytes, err := x509.CreateCertificate(rand.Reader, serverCert, caCert, serverKey.Public(), caKey)
+			Expect(err).To(BeNil())
+			expiredPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certBytes})
+
+			secret.Data[common.ServerCertPem] = expiredPEM
+			Expect(k8sClient.Update(ctx, secret)).To(Succeed())
+
+			By("Creating a new cert provider and synchronize should generate new certificates")
+			cp := certificate.NewProvider(k8sClient, secretName, secretNamespace)
+			Expect(cp.SyncSecret(context.TODO(), secretName, secretNamespace)).To(Succeed())
+
+			By("Checking out whether the webhook certificate changed after syncronize")
+			serverPEM, err := cp.ServerCert()
+			Expect(err).To(BeNil())
+			Expect(serverPEM).ShouldNot(Equal(expiredPEM))
 		})
 	})
 })
