2b6c200cdfc099cdebd724b3e9062773352b9469
Yi Chen <github@chenyicn.net>
2025-04-21 16:16:52 +0800

    Update imports
    Signed-off-by: Yi Chen <github@chenyicn.net>

--- internal/webhook/suite_test.go
+++ internal/webhook/suite_test.go
@@ -30,6 +30,7 @@ import (
 	. "github.com/onsi/gomega"
 
 	admissionv1 "k8s.io/api/admission/v1"
+	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -41,7 +42,6 @@ import (
 
 	"github.com/kubeflow/spark-operator/api/v1beta1"
 	"github.com/kubeflow/spark-operator/api/v1beta2"
-	"github.com/kubeflow/spark-operator/pkg/client/clientset/versioned/scheme"
 	// +kubebuilder:scaffold:imports
 )
 
