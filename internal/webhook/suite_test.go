42f42bc1793698c84aa1e5c480e7c4da5abb763e
Joshua Cuellar <joshuac.cuellar@outlook.com>
2025-07-08 22:24:38 +0000

    Update to Kubernetes v1.33.0
    Signed-off-by: Joshua Cuellar <joshuac.cuellar@outlook.com>

--- internal/webhook/suite_test.go
+++ internal/webhook/suite_test.go
@@ -75,7 +75,7 @@ var _ = BeforeSuite(func() {
 		// Note that you must have the required binaries setup under the bin directory to perform
 		// the tests directly. When we run make test it will be setup and used automatically.
 		BinaryAssetsDirectory: filepath.Join("..", "..", "..", "bin", "k8s",
-			fmt.Sprintf("1.32.0-%s-%s", runtime.GOOS, runtime.GOARCH)),
+			fmt.Sprintf("1.33.0-%s-%s", runtime.GOOS, runtime.GOARCH)),
 
 		WebhookInstallOptions: envtest.WebhookInstallOptions{
 			Paths: []string{filepath.Join("..", "..", "config", "webhook")},
