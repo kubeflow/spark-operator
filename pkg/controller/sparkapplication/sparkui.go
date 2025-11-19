33411fd6637d1925a71d54f795920b42ab167c80
gtrawinski <gracjan.trawinski@relativity.com>
2025-11-19 17:53:51 +0100

    revert: removal of error

--- pkg/controller/sparkapplication/sparkui.go
+++ pkg/controller/sparkapplication/sparkui.go
@@ -70,10 +70,10 @@ func getUITargetPort(app *v1beta2.SparkApplication) (int32, error) {
 	if ok {
 		port, err := strconv.Atoi(portStr)
 		if err != nil {
-			return defaultSparkWebUIPort, nil
+			return defaultSparkWebUIPort, err
 		}
 		if port < math.MinInt32 || port > math.MaxInt32 {
-			return defaultSparkWebUIPort, nil
+			return defaultSparkWebUIPort, fmt.Errorf("Spark UI port %d out of int32 bounds", port)
 		}
 		return int32(port), nil
 	}
