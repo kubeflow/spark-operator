96ccf000e7c0afa9f9d7084b6ebd64d9b54aa18a
Rohith Peddi <rohith18p18@gmail.com>
2026-08-27 07:57:38 +0530

    refactor: centralize Spark container selection (#3115)
    * refactor: centralize Spark container selection
    
    Signed-off-by: rohith1X8 <rohith18p18@gmail.com>
    
    * chore: address review comments
    
    Signed-off-by: rohith1X8 <rohith18p18@gmail.com>
    
    ---------
    
    Signed-off-by: rohith1X8 <rohith18p18@gmail.com>

---
+++ pkg/util/container_test.go
@@ -0,0 +1,70 @@
+/*
+Copyright The Kubeflow Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package util_test
+
+import (
+	"github.com/kubeflow/spark-operator/v2/pkg/util"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+)
+
+var _ = Describe("GetContainerByNameOrFirst", func() {
+	It("returns nil when input container list is empty", func() {
+		Expect(util.GetContainerByNameOrFirst(nil, "spark")).To(BeNil())
+	})
+
+	It("returns the named container when it exists", func() {
+		containers := []corev1.Container{
+			{Name: "sidecar", Image: "busybox"},
+			{Name: "spark", Image: "apache/spark"},
+		}
+
+		container := util.GetContainerByNameOrFirst(containers, "spark")
+
+		Expect(container).NotTo(BeNil())
+		Expect(container.Name).To(Equal("spark"))
+		Expect(container.Image).To(Equal("apache/spark"))
+	})
+
+	It("returns the first container when the named container is absent", func() {
+		containers := []corev1.Container{
+			{Name: "first", Image: "first-image"},
+			{Name: "second", Image: "second-image"},
+		}
+
+		container := util.GetContainerByNameOrFirst(containers, "spark")
+
+		Expect(container).NotTo(BeNil())
+		Expect(container.Name).To(Equal("first"))
+		Expect(container.Image).To(Equal("first-image"))
+	})
+
+	It("returns a pointer to the original slice element", func() {
+		containers := []corev1.Container{
+			{Name: "first"},
+			{Name: "spark"},
+		}
+
+		container := util.GetContainerByNameOrFirst(containers, "spark")
+		Expect(container).NotTo(BeNil())
+
+		container.Image = "updated-image"
+
+		Expect(containers[1].Image).To(Equal("updated-image"))
+	})
+})
